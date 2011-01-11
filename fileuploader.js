900e47a4940a29259345060197a29f4750445ac7
bgrins <briangrinstead@gmail.com>
2011-01-11 11:43:27 -0600

    remove fileupload.js, move the upload functionality into the plugin only

--- fileuploader.js
+++ fileuploader.js
@@ -10,12 +10,14 @@ See http://github.com/bgrins/filereader.js for documentation
 (function(global) {
 
 global.FileUploader = uploader;
+global.FileUploader.doUpload = doUpload;
 global.FileUploader.opts = {
 	thumbnail: [50, 50],
 	dragClass: 'drag',
 	outputClass: 'file-list',
 	fileinput: 'file-input',
 	dropzone: 'dropzone',
+	debug: true,
 	errors: {
 		fileTooLarge: '{filename} is too large.'
 	}
@@ -81,39 +83,38 @@ function uploader(form, opts) {
 	};
 	
 	
+	function sendFile(file, li) {
+		var sendMessage = li.find(".send");
+		sendMessage.html("Uploading...");
+	
+		var uploadopts = {
+			data: {
+				"name": "value"
+			},
+			url: o.url,
+			onload: function(e) {
+				log("loaded", e);
+				sendMessage.html("Done!");
+			},
+			onprogress: function(e, file, percentage) {
+				li.find(".progress div").css("width", percentage + "%");
+			},
+			onerror: function(e) {
+				log("error", e);
+				sendMessage.html("Error!");
+			}
+		};
+	
+		doUpload(file, uploadopts);
+	}
+	
+	function log() {
+		if (o.debug && console) { console.log(Array.prototype.slice.call(arguments)); }
+	};
 
 	FileReaderJS.setupInput(fileinput[0], opts);
 	FileReaderJS.setupDrop(dropzone[0], opts);
 }
-
-
-
-function sendFile(file, li) {
-	var sendMessage = li.find(".send");
-	sendMessage.html("Uploading...");
-
-	var uploadopts = {
-		data: {
-			"name": "value"
-		},
-		url: "upload.php",
-		onload: function(e) {
-			log("loaded", e);
-			sendMessage.html("Done!");
-		},
-		onprogress: function(e, file, percentage) {
-			li.find(".progress div").css("width", percentage + "%");
-		},
-		onerror: function(e) {
-			log("error", e);
-			sendMessage.html("Error!");
-		}
-	};
-
-	FileUploadJS.send(file, uploadopts);
-}
-
-
 
 function groupTemplate(groupID, files) {
 	var html = [];
@@ -136,8 +137,6 @@ function groupTemplate(groupID, files) {
 	
 	return  start + html.join('');
 }
-
-
 
 function getThumbnail(img, MAX_WIDTH, MAX_HEIGHT) {
 
@@ -163,9 +162,46 @@ function getThumbnail(img, MAX_WIDTH, MAX_HEIGHT) {
     
 	return canvas;
 }
-
-
-
+	
+function doUpload(file, opts) {
+    
+    // Default option callbacks
+    opts = opts || { };
+    opts.onprogress = opts.onprogress || function() { };
+    opts.onerror = opts.onerror || function() { };
+    opts.onload = opts.onload || function() { };
+    opts.url = opts.url || "upload.php";
+    opts.data = opts.data || { };
+    opts.fileInputName = opts.fileInputName || "file";
+    
+    
+    // Generate FormData object
+    var fd = new FormData();
+    for (var d in opts.data) { fd.append(d, opts[d]); }
+    fd.append(opts.fileInputName, file);
+    
+    // Send off request
+    var xhr = new XMLHttpRequest();
+    xhr.upload.addEventListener("progress", onprogress, false);
+    xhr.upload.addEventListener("load", onload, false);
+    xhr.upload.addEventListener("error", onerror, false);
+    xhr.open("POST", opts.url);
+    xhr.send(fd);
+    
+    
+    function onprogress(e) {
+    	if (e.lengthComputable) {
+        	var percentage = Math.round((e.loaded * 100) / e.total);
+        	opts.onprogress(e, file, percentage);
+    	}
+    }
+    function onload(e) {
+        opts.onload(e, file);
+    }
+    function onerror(e) {
+        opts.onerror(e, file);
+    }
+}
 
 $.fn.FileUploader = function(opts) {
 	return this.each(function() {		
