76c84ce1eafdf42fce02a1de046e19b20cabd6e5
bgrins <briangrinstead@gmail.com>
2012-05-26 12:52:22 -0500

    cleanup

--- filereader.js
+++ filereader.js
@@ -162,40 +162,40 @@
 
         var instanceOptions = extend(extend({}, FileReaderJS.opts), opts);
         var dragClass = instanceOptions.dragClass;
+        var initializedOnBody = false;
 
-        // Bind to body to prevent custom events from firing when it was initialized on the page.
-        document.body.addEventListener("dragstart", globaldragstart, true);
-        document.body.addEventListener("dragend", globaldragend, true);
-        document.body.addEventListener("drop", preventFileRedirect, false);
-
+        // Bind drag events to the dropbox to add the class while dragging, and accept the drop data transfer.
         dropbox.addEventListener("dragenter", onlyWithFiles(dragenter), false);
         dropbox.addEventListener("dragleave", onlyWithFiles(dragleave), false);
         dropbox.addEventListener("dragover", onlyWithFiles(dragover), false);
         dropbox.addEventListener("drop", onlyWithFiles(drop), false);
 
-        var initializedOnBody = false;
-        function onlyWithFiles(fn) {
-            return function() {
-                if (initializedOnBody) {
-                    return;
-                }
-                fn.apply(this, arguments);
-            }
-        }
+        // Bind to body to prevent the dropbox events from firing when it was initialized on the page.
+        document.body.addEventListener("dragstart", bodydragstart, true);
+        document.body.addEventListener("dragend", bodydragend, true);
+        document.body.addEventListener("drop", bodydrop, false);
 
-        function globaldragend(e) {
+        function bodydragend(e) {
             initializedOnBody = false;
         }
 
-        function globaldragstart(e) {
+        function bodydragstart(e) {
             initializedOnBody = true;
         }
 
-        function preventFileRedirect(e) {
+        function bodydrop(e) {
             if (e.dataTransfer.files && e.dataTransfer.files.length ){
                 e.stopPropagation();
                 e.preventDefault();
             }
+        }
+
+        function onlyWithFiles(fn) {
+            return function() {
+                if (!initializedOnBody) {
+                    fn.apply(this, arguments);
+                }
+            };
         }
 
         function drop(e) {
@@ -206,24 +206,27 @@
             }
             processFileList(e.dataTransfer.files, instanceOptions);
         }
+
         function dragenter(e) {
+            e.stopPropagation();
+            e.preventDefault();
             if (dragClass) {
                 addClass(dropbox, dragClass);
             }
-            e.stopPropagation();
-            e.preventDefault();
         }
+
         function dragleave(e) {
             if (dragClass) {
                 removeClass(dropbox, dragClass);
             }
         }
+
         function dragover(e) {
+            e.stopPropagation();
+            e.preventDefault();
             if (dragClass) {
                 addClass(dropbox, dragClass);
             }
-            e.stopPropagation();
-            e.preventDefault();
         }
     }
 
